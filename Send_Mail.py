1afe058c63ec826ea0da246b9e13e68cf66feb86
Ozgur <49453238+tiskayaozgur@users.noreply.github.com>
2021-05-21 14:22:53 +0300

    Send_Mail.py
    Send_Mailv2.py

--- Send_Mail.py
+++ Send_Mail.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-# import tkinter.messagebox
+import tkinter.messagebox
 from tkinter import *
 import smtplib
 from email.mime.text import MIMEText
@@ -10,7 +10,6 @@ class GUI:
         master.geometry('500x500')
         master.title('Send Email')
         self.master = master
-
 
         # creating username label-entries
         self.username_label = tk.Label(self.master, text='Username', bg='grey')
@@ -24,33 +23,31 @@ class GUI:
         self.password_entry = tk.Entry(self.master, show='*') #for cursor invisible, i used show parameter.
         self.password_entry.grid(row=1, column=1, sticky='nsew')
 
-        # # creating from label-entries
-        # self.from_label = tk.Label(self.master, text='From', bg='grey')
-        # self.from_label.grid(row=2, column=0, sticky='nsew')
-        # self.from_entry = tk.Entry(self.master)
-        # self.from_entry.grid(row=2, column=1, sticky='nsew')
-
         # creating to label-entries
         self.to_label = tk.Label(self.master, text='To', bg='grey')
-        self.to_label.grid(row=3, column=0, sticky='nsew')
+        self.to_label.grid(row=2, column=0, sticky='nsew')
         self.to_entry = tk.Entry(self.master)
-        self.to_entry.grid(row=3, column=1, sticky='nsew')
+        self.to_entry.grid(row=2, column=1, sticky='nsew')
 
         # creating subject label-entries
         self.subject_label = tk.Label(self.master, text='Subject', bg='grey')
-        self.subject_label.grid(row=4, column=0, sticky='nsew')
+        self.subject_label.grid(row=3, column=0, sticky='nsew')
         self.subject_entry = tk.Entry(self.master)
-        self.subject_entry.grid(row=4, column=1, sticky='nsew')
+        self.subject_entry.grid(row=3, column=1, sticky='nsew')
 
         # creating content label-entries
         self.content_label = tk.Label(self.master, text='Content', bg='grey')
-        self.content_label.grid(row=5, column=0, sticky='nsew')
+        self.content_label.grid(row=4, column=0, sticky='nsew')
         self.content_entry = tk.Entry(self.master)
-        self.content_entry.grid(row=5, column=1, sticky='nsew')
+        self.content_entry.grid(row=4, column=1, sticky='nsew')
 
         # Creating add button in bottom_right_frame
         self.button_send = tk.Button(self.master, text="SEND", command=self.button_send_handler)
-        self.button_send.grid(row=6, column=0, columnspan=2)
+        self.button_send.grid(row=5, column=0, columnspan=2)
+
+        #creating clear all button for deleting all datas in entries
+        self.button_clear_all = tk.Button(self.master, text="CLEAR ALL", command=self.button_clear_all_handler)
+        self.button_clear_all.grid(row=5, column=1, columnspan=2)
 
 
 
@@ -61,11 +58,10 @@ class GUI:
         # setting every entries row's heavy
         self.master.rowconfigure(0, weight=1)
         self.master.rowconfigure(1, weight=1)
-        # self.master.rowconfigure(2, weight=1)
+        self.master.rowconfigure(2, weight=1)
         self.master.rowconfigure(3, weight=1)
         self.master.rowconfigure(4, weight=1)
         self.master.rowconfigure(5, weight=1)
-        self.master.rowconfigure(6, weight=1)
 
 
     def button_send_handler(self):
@@ -78,25 +74,36 @@ class GUI:
         subject = self.subject_entry.get().strip()
         msg = self.content_entry.get().strip()
 
-        #creating MIME areas
-        mt = MIMEText(msg)
-        mt['From'] = user_name
-        mt['To'] = receipants
-        mt['Subject'] = subject
+        if user_name != "" and password != "" and receipants != "" and subject != "" and msg != "":
+            #creating MIME areas
+            mt = MIMEText(msg)
+            mt['From'] = user_name
+            mt['To'] = receipants
+            mt['Subject'] = subject
 
-        msg = mt.as_string()
+            msg = mt.as_string()
 
-        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        server.ehlo()
-        server.login(user_name, password)
-        server.sendmail(user_name, receipants, msg)
-        server.quit()
+            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            server.ehlo()
+            server.login(user_name, password)
+            server.sendmail(user_name, receipants, msg)
+            server.quit()
 
-        #After sending, delete all data's from GUI
+            #After sending, delete all data's from GUI
+            self.username_entry.delete(0, tk.END)
+            self.password_entry.delete(0, tk.END)
+            self.to_entry.delete(0, tk.END)
+            self.subject_entry.delete(0, tk.END)
+            self.content_entry.delete(0, tk.END)
+
+        else:
+            messagebox.showwarning(title="Warning", message="Please enter all entries to send email!")
+
+    def button_clear_all_handler(self):
+        # Delete all datas from every entries using clear all button
         self.username_entry.delete(0, tk.END)
         self.password_entry.delete(0, tk.END)
         self.to_entry.delete(0, tk.END)
-        # self.from_entry.delete(0, tk.END)
         self.subject_entry.delete(0, tk.END)
         self.content_entry.delete(0, tk.END)
 
@@ -104,4 +111,3 @@ class GUI:
 root = tk.Tk()
 gdb = GUI(root)
 root.mainloop()
-print("Gönderim tamamlandı")
