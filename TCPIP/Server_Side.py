036f4406bde487e4a46323769b1ec6caba53409f
Ozgur <49453238+tiskayaozgur@users.noreply.github.com>
2021-05-26 22:22:06 +0300

    Server_Side.py
    New Version

--- TCPIP/Server_Side.py
+++ TCPIP/Server_Side.py
@@ -7,7 +7,7 @@ from tkinter import *
 import tkinter.font as tkFont
 import tkinter.scrolledtext
 import socket
-
+import time
 
 class GUI_Server:
     def __init__(self, master):
@@ -53,51 +53,48 @@ class GUI_Server:
         self.button_send = tk.Button(self.right_frame, text="DELETE", command=self.button_delete_handler)
         self.button_send.pack(side='left', expand=True)
 
-        # #Creating connect button in right frame
-        # self.button_connect = tk.Button(self.right_frame, text="CONNECT", command=self.button_connect_handler)
-        # self.button_connect.pack(side='left', expand=True)
+        #Creating connect button in right frame
+        self.button_connect = tk.Button(self.right_frame, text="CONNECT", command=self.button_connect_handler)
+        self.button_connect.pack(side='left', expand=True)
 
-        # connecting requeriments
-        SERVER_PORT_NO = 50050
-        self.server_sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)
-        self.server_sock.bind(("", SERVER_PORT_NO))
-        self.server_sock.listen(32)
+    #for connecting, you should use conect button
+    def button_connect_handler(self):
+        try:
+            # connecting requeriments
+            SERVER_PORT_NO = 50050
+            self.server_sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)
+            self.server_sock.bind(("", SERVER_PORT_NO))
+            self.server_sock.listen(32)
 
-        print("Waiting for connection...")
-        self.client_sock, self.client_addr = self.server_sock.accept()
-        print(f"Server'a baglanan client'in ip-port bilgisi={self.client_addr}")
+            print("Waiting for connection...")
+            self.client_sock, self.client_addr = self.server_sock.accept()
+            print(f"Server'a baglanan client'in ip-port bilgisi={self.client_addr}")
 
-
-
-
-        while True:
-            # showing all datas that client sent
-            binary_client = self.client_sock.recv(1024)  # Clienttan gelen byte nesneyı recv ıle aldım.
-            text_client = binary_client.decode("UTF-8")  # byte nesnesını strye cevırdım, cunku bu yazıyı ters cevırıp karsı tarafa gonderecegım
-            if text_client != "":
-                print(text_client)
-                self.text_chat.insert(tk.END,"<Client>" + text_client + '\n')  # clienttan gelen mesajı guımıze yerlestırdık
-            else:
+            while True:
+                # showing all datas that client sent
+                binary_client = self.client_sock.recv(1024)  # Clienttan gelen byte nesneyı recv ıle aldım.
+                text_client = binary_client.decode("UTF-8")  # byte nesnesını strye cevırdım, cunku bu yazıyı ters cevırıp karsı tarafa gonderecegım
+                if text_client != "":
+                    print(text_client)
+                    self.text_chat.insert(tk.END, "<Client>" + text_client + '\n')  # clienttan gelen mesajı guımıze yerlestırdık
                 break
 
 
 
-
+        except Exception as e:
+            print(e)
 
 
     def button_send_handler(self):
-        # # taking datas from server entries
-        # text_server = self.entry.get()
-        #
-        # # showing all datas that server sent
-        # self.text_chat.insert(tk.END, "<Server>" + text_server + '\n')
-        #
-        # b = text_server.encode("UTF-8")  # Gırılen str'yı byte nesnesıne cevırdım kı bunu send ıle server tarafına gonderebıleyım.
-        # self.client_sock.send(b)
-        # self.entry.delete(0, tk.END)
+        # taking datas from server entries
+        text_server = self.entry.get()
 
-        pass
+        # showing all datas that server sent
+        self.text_chat.insert(tk.END, "<Server>" + text_server + '\n')
 
+        b = text_server.encode("UTF-8")  # Gırılen str'yı byte nesnesıne cevırdım kı bunu send ıle server tarafına gonderebıleyım.
+        self.client_sock.send(b)
+        self.entry.delete(0, tk.END)
 
 
 
